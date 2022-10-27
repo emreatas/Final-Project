d8db55d994ff8f86c7fdb038ad8b00b3c610300c
Saidt <saidsoykan@outlook.com>
2022-10-27 17:57:59 +0300

    Tried Something

---
+++ Final Project/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    Animator anim;
+    public void PlayWalkAnimation()
+    {
+        anim.SetBool("");
+    }
+}
