7ba84a8a2fd51a94a4d5d7af08630474f97bacd5
Serkan <yilserkan123@gmail.com>
2022-10-31 14:42:00 +0300

    created GameEvent struct
    created a class for events to prevent possible mistakes (+= , -=)  whilst subscribing or unsubscribing from events.

---
+++ Final Project/Assets/Scripts/Utils/GameEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Utils
+{
+    public struct GameEvent
+    {
+        public event Action m_Action;
+
+        public void Invoke()
+        {
+            m_Action?.Invoke();
+        }
+
+        public void AddListener(Action action)
+        {
+            m_Action += action;
+        }
+
+        public void RemoveListener(Action action)
+        {
+            m_Action -= action;
+        }
+    }
+    
+    public struct GameEvent<T>
+    {
+        public event Action<T> m_Action;
+
+        public void Invoke(T data)
+        {
+            m_Action?.Invoke(data);
+        }
+
+        public void AddListener(Action<T> action)
+        {
+            m_Action += action;
+        }
+
+        public void RemoveListener(Action<T> action)
+        {
+            m_Action -= action;
+        }
+    }
+}
