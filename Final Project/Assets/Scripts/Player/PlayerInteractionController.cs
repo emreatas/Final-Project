f8a8994280ba665c1013b8d632af03fec851ae09
Serkan <yilserkan123@gmail.com>
2022-10-31 14:40:12 +0300

    created interaction controller

---
+++ Final Project/Assets/Scripts/Player/PlayerInteractionController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Interacbles;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerInteractionController : MonoBehaviour
+    {
+        [SerializeField] private PlayerInputSystem inputSystem;
+        [SerializeField] private LayerMask interactableLayerMask;
+
+        private void OnEnable()
+        {
+            AddListeners();
+        }
+
+        private void OnDisable()
+        {
+            RemoveListeners();
+        }
+
+        private void HandleOnInteractionStarted()
+        {
+            
+            Debug.Log("Raycast Hit started");
+            
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, 10, interactableLayerMask);
+      
+            for (int i = 0; i < hitColliders.Length; i++)
+            {
+                hitColliders[i].GetComponent<IInteractable>().Interact();
+            }
+        }
+        
+
+        private void AddListeners()
+        {
+           inputSystem.OnBasicAttackStarted.AddListener(HandleOnInteractionStarted);
+        }
+
+        private void RemoveListeners()
+        {
+            inputSystem.OnBasicAttackStarted.RemoveListener(HandleOnInteractionStarted);
+        }
+    }
+}
