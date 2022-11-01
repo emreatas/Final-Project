7e5b472c3e577a08e611cd01c450afef56738b5d
Serkan <yilserkan123@gmail.com>
2022-11-01 19:04:26 +0300

    modified idle state
    State machine switches now also whilst the player is pressing the move button to move state. This solved the issue that the player could hold the move button whilst he was performing a skill. But the state machine wont switch to idle until the skill was finished so when it swtiches to idle state after the attack state it can't catcht the move start event in some occassions

--- Final Project/Assets/Scripts/State Machine/IdleState.cs
+++ Final Project/Assets/Scripts/State Machine/IdleState.cs
@@ -9,16 +9,23 @@ namespace StateMachine
         public override void OnEnter()
         {
             Debug.Log("Play Idle Animation");
+            m_StateMachine.AnimationController.PlayIdleAnimation();
             AddListeners();
         }
 
         public override void OnExit()
         {
             Debug.Log("Stop Idle Animation");
+            m_StateMachine.AnimationController.StopIdleAnimation();
             RemoveListeners();
         }
 
         private void HandleOnMoveStarted(Vector3 movementVector)
+        {
+            m_StateMachine.SwitchState(PlayerStates.Move);
+        }
+        
+        private void HandleOnMovePerformed(Vector3 movementVector)
         {
             m_StateMachine.SwitchState(PlayerStates.Move);
         }
@@ -35,6 +42,7 @@ namespace StateMachine
         private void AddListeners()
         {
             m_StateMachine.InputSystem.OnMoveStarted.AddListener(HandleOnMoveStarted);
+            m_StateMachine.InputSystem.OnMovePerformed.AddListener(HandleOnMovePerformed);
             
             m_StateMachine.InputSystem.OnBasicAttackStarted.AddListener(HandleOnAttackStart);
             m_StateMachine.InputSystem.OnPrimarySkillStarted.AddListener(HandleOnAttackStart);
@@ -44,6 +52,7 @@ namespace StateMachine
         private void RemoveListeners()
         {
             m_StateMachine.InputSystem.OnMoveStarted.RemoveListener(HandleOnMoveStarted);
+            m_StateMachine.InputSystem.OnMovePerformed.RemoveListener(HandleOnMovePerformed);
             
             m_StateMachine.InputSystem.OnBasicAttackStarted.RemoveListener(HandleOnAttackStart);
             m_StateMachine.InputSystem.OnPrimarySkillStarted.RemoveListener(HandleOnAttackStart);
