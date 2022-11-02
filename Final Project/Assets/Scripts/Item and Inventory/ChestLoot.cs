e6014cf11165865a76795dc9aaad24e6d9dfd2e3
Serkan <yilserkan123@gmail.com>
2022-11-02 17:30:55 +0300

    created chest loot scriptable object and assets

---
+++ Final Project/Assets/Scripts/Item and Inventory/ChestLoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+namespace Items
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Chest")]
+    public class ChestLoot : ScriptableObject
+    {
+        public List<Item> dropableItem;
+
+        public Dictionary<Item, GameObject> instansiatedItems = new Dictionary<Item, GameObject>();
+
+        public void SelectItemFromChest(Item item)
+        {
+            instansiatedItems.Remove(item);
+            dropableItem.Remove(item);
+        }
+    }
+}
