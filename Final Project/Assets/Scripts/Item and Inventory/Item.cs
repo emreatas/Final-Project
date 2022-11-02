9091b767601c9fcf217dd376190eb50850f7315e
Emre Ataş <emreatas1@hotmail.com>
2022-11-02 13:11:15 +0300

    adjustment

--- Final Project/Assets/Scripts/Item and Inventory/Item.cs
+++ Final Project/Assets/Scripts/Item and Inventory/Item.cs
@@ -6,5 +6,15 @@ public class Item : ScriptableObject
 {
     public string ItemName = "New Item";
     public Sprite Icon = null;
+    public ItemTier tier;
     public bool isDefaultItem = false;
+}
+
+public enum ItemTier
+{
+    NoTier,
+    Standart,
+    Rare,
+    Epic,
+    Legendary
 }
