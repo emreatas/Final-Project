e037d677ea47c2560e1f2735169e1c85896e5e74
Serkan <yilserkan123@gmail.com>
2022-11-02 17:38:56 +0300

    refactored interacition controller and fixed bug
    Fixed null exception error in method HandleOnInteractionStarted

--- Final Project/Assets/Scripts/Player/PlayerInteractionController.cs
+++ Final Project/Assets/Scripts/Player/PlayerInteractionController.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
-using Interacbles;
-using MyNamespace;
+using Interactables;
 using UnityEngine;
 using Utils;
 
@@ -10,10 +9,9 @@ namespace Player
     {
         [SerializeField] private PlayerInputSystem inputSystem;
         [SerializeField] private LayerMask interactableLayerMask;
-        [SerializeField] private float interactRadius;
         [SerializeField] private string interactableTag;
         
-        private List<IInteractable> interactableChest = new List<IInteractable>();
+        private List<Interactable> interactables = new List<Interactable>();
 
         private void OnEnable()
         {
@@ -29,9 +27,10 @@ namespace Player
         {
             if (other.gameObject.CompareTag(interactableTag))
             {
-                // CanvasManager.instance.OnInteractableStart(true);   
                 InteractableUI.Instance.EnableInteractButton();
-                interactableChest.Add(other.GetComponent<IInteractable>());
+
+                var interactable = other.GetComponent<Interactable>();
+                interactables.Add(interactable);
             }
         }
 
@@ -39,26 +38,28 @@ namespace Player
         {
             if (other.gameObject.CompareTag(interactableTag))
             {
-                interactableChest.Remove(other.GetComponent<IInteractable>());
-                if (interactableChest.Count == 0)
+                var interactable = other.GetComponent<Interactable>();
+                interactables.Remove(interactable);
+                
+                if (interactables.Count == 0)
                 {
+                    InteractableUI.Instance.DisableInteractPanel();
                     InteractableUI.Instance.DisableInteractButton();
-                    // CanvasManager.instance.OnInteractableStart(false);
                 }
             }
         }
         
         private void HandleOnInteractionStarted()
         {
-            Debug.Log("Interaction started");
-
-            for (int i = 0; i < interactableChest.Count; i++)
+            for (int i = 0; i < interactables.Count; i++)
             {
-                interactableChest[i].Interact();
+                if (interactables[i] != null)
+                {
+                    interactables[i].Interact();
+                }
             }
         }
-
-
+        
         private void AddListeners()
         {
             inputSystem.OnInteractStarted.AddListener(HandleOnInteractionStarted);
