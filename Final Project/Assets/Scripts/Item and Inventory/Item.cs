0e2811a53207c8cc83a4978f6ff39e4567b09d40
Serkan <yilserkan123@gmail.com>
2022-11-02 13:27:04 +0300

    Item drop system

--- Final Project/Assets/Scripts/Item and Inventory/Item.cs
+++ Final Project/Assets/Scripts/Item and Inventory/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 
@@ -8,7 +9,28 @@ public class Item : ScriptableObject
     public Sprite Icon = null;
     public ItemTier tier;
     public bool isDefaultItem = false;
+
+    public Color GetTierColor()
+    {
+        switch (tier)
+        {
+            case ItemTier.NoTier:
+                return new Color(0,0,0,1);
+            case ItemTier.Standart:
+                return Color.green;
+            case ItemTier.Rare:
+                return Color.blue;
+            case ItemTier.Epic:
+                return Color.magenta;
+            case ItemTier.Legendary:
+                return Color.yellow;
+            default:
+                return new Color(0,0,0,1);
+        }
+    }
 }
+
+
 
 public enum ItemTier
 {
@@ -18,3 +40,4 @@ public enum ItemTier
     Epic,
     Legendary
 }
+
