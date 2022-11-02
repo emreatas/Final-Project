86750ba0c0efea3a37bbb9983d9ad4c46c9f6c06
Emre Ataş <emreatas1@hotmail.com>
2022-11-02 10:52:58 +0300

    Chest script added

---
+++ Final Project/Assets/Scripts/Item and Inventory/Chest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Interacbles;
+public class Chest : MonoBehaviour, IInteractable
+{
+    public List<Item> dropableItem = new List<Item>();
+
+    public void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+
+
+}
