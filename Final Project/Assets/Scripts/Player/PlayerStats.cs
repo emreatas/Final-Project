85e96bae693895cf2cc790478a729294e61b4a52
Serkan <yilserkan123@gmail.com>
2022-11-04 17:54:50 +0300

    created test player stats script

---
+++ Final Project/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Stat;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerStats : MonoBehaviour
+    {
+        [SerializeField] private CharacterStat characterStats;
+        [SerializeField] private StatType targetStat;
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                AttributeModifier att = new AttributeModifier(100,targetStat);
+                characterStats.AddModifier(att);
+            }
+        }
+    }
+}
