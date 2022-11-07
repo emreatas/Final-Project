549da336d3550b671bc9a2bbccfb4b3648c1956d
Serkan <yilserkan123@gmail.com>
2022-11-07 16:21:14 +0300

    modified bug in chest loot
    Chest loot were not instansiating new items but using the default scritable objects

--- Final Project/Assets/Scripts/Item and Inventory/ChestLoot.cs
+++ Final Project/Assets/Scripts/Item and Inventory/ChestLoot.cs
@@ -14,6 +14,14 @@ namespace Items
         [SerializeField] private int maxRandomItemDrop;
         public List<Item> GetRandomLoot()
         {
+            List<Item> dropItemList = new List<Item>();
+            //dropItemList.AddRange(constantDropItems);
+
+            for (int i = 0; i < constantDropItems.Count; i++)
+            {
+                dropItemList.Add(Instantiate(constantDropItems[i]));
+            }
+            
             float totalChance = 0;
             for (int i = 0; i < randomDropItems.Count; i++)
             {
@@ -21,10 +29,6 @@ namespace Items
             }
 
             int randomItemDropCount = UnityEngine.Random.Range(minRandomItemDrop, maxRandomItemDrop+1);
-
-            List<Item> dropItemList = new List<Item>();
-            dropItemList.AddRange(constantDropItems);
-            
             for (int i = 0; i < randomItemDropCount; i++)
             {
                 float randomNumber = UnityEngine.Random.Range(0, totalChance);
@@ -34,10 +38,15 @@ namespace Items
                     randomNumber -= randomDropItems[j].DropChance;
                     if (randomNumber <= 0)
                     {
-                        dropItemList.Add(randomDropItems[j].Item);
+                        dropItemList.Add(Instantiate(randomDropItems[j].Item));
                         break;
                     }
                 }
+            }
+
+            for (int i = 0; i < dropItemList.Count; i++)
+            {
+                dropItemList[i].GetRandomStats();
             }
             
             return dropItemList;
