2142dd4176b3be07805165c21d5b3e41f9b29643
Serkan <yilserkan123@gmail.com>
2022-11-09 16:32:18 +0300

    created movement controller

--- Final Project/Assets/Scripts/Player/PlayerMovementController.cs
+++ Final Project/Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using MEC;
 using UnityEngine;
 
 namespace Player
@@ -8,6 +9,7 @@ namespace Player
     {
         [SerializeField] private PlayerMovementSettings movementSettings;
         [SerializeField] private Rigidbody rb;
+        [SerializeField] private float lerpRotationDuration;
         
         public void MovePlayer(Vector3 movementVector)
         {
@@ -27,6 +29,31 @@ namespace Player
                 movementSettings.RotationSpeed * Time.deltaTime);
             
             rb.MoveRotation(newRot);
+        }
+
+        public void LerpPlayerRotation(Vector3 targetPos)
+        {
+            if (targetPos != Vector3.zero)
+            {
+                Timing.RunCoroutine(_LerpRotation(targetPos));
+            }
+        }
+
+        IEnumerator<float> _LerpRotation(Vector3 targetPos)
+        {
+            float lerpedTime = 0;
+            
+            Quaternion startRot = rb.rotation;
+            Quaternion targetRot = Quaternion.LookRotation(targetPos - rb.position);
+            
+            while (lerpRotationDuration > lerpedTime)
+            {
+                rb.rotation = Quaternion.Slerp(startRot, targetRot, lerpedTime/lerpRotationDuration);        
+        
+                lerpedTime += Time.deltaTime;
+                    
+                yield return Timing.WaitForOneFrame;
+            }
         }
     }
 }
