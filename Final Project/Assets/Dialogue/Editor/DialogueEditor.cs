3a0cc3873dbb634c107abb3abc0b63307428ab43
Berkay Yelekçi <mberkay.yelekci@gmail.com>
2022-11-09 14:33:14 +0300

    callback added

--- Final Project/Assets/Dialogue/Editor/DialogueEditor.cs
+++ Final Project/Assets/Dialogue/Editor/DialogueEditor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-
+using UnityEditor.Callbacks;
 
 
 namespace RPG.Dialogue.Editor
@@ -12,8 +12,21 @@ namespace RPG.Dialogue.Editor
        [MenuItem("Window/Dialogue Editor")]
        public static void ShowEditorWindow()
         {
-            GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+            GetWindow(typeof(DialogueEditor), true, "Dialogue Editor");
         }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            if (dialogue != null)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
+        }
+
     }
 }
 
