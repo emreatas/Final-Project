f19cbbb04aadcaca49b21e41e0cffe52ad623713
Serkan <yilserkan123@gmail.com>
2022-11-09 15:34:40 +0300

    created skill indicator custom editor

---
+++ Final Project/Assets/Scripts/Custom Editor/SkillIndicatorSettings_Editor.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Skills;
+using UnityEditor;
+using UnityEngine;
+
+namespace CustomEditor
+{
+    [UnityEditor.CustomEditor(typeof(SkillIndicatorSettings))]
+    public class SkillIndicatorSettings_Editor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            SkillIndicatorSettings skillSettings = (SkillIndicatorSettings)target;
+
+            EditorGUILayout.LabelField("Radius");
+            skillSettings.HasRadius = EditorGUILayout.Toggle("Has Radius", skillSettings.HasRadius);
+            if (skillSettings.HasRadius)
+            {
+                skillSettings.radius = EditorGUILayout.FloatField("Radius", skillSettings.radius);
+                skillSettings.radiusColor = EditorGUILayout.ColorField("Radius Color", skillSettings.radiusColor);
+                skillSettings.radiusSprite = EditorGUILayout.ObjectField("Radius Sprite", skillSettings.radiusSprite, typeof(Sprite), true) as Sprite;
+                
+                
+                
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Impact");
+                skillSettings.HasImpact = EditorGUILayout.Toggle("Has Impact", skillSettings.HasImpact);
+                if (skillSettings.HasImpact)
+                {
+                    skillSettings.impactRadius = EditorGUILayout.FloatField("Impact Radius", skillSettings.impactRadius);
+                    skillSettings.impactColor = EditorGUILayout.ColorField("Impact Color", skillSettings.impactColor);
+                    skillSettings.impactSprite = EditorGUILayout.ObjectField("Impact Sprite", skillSettings.impactSprite, typeof(Sprite), true) as Sprite;
+                    
+                }
+            }
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Direction");
+            skillSettings.HasDirection = EditorGUILayout.Toggle("Has Direction", skillSettings.HasDirection);
+            if (skillSettings.HasDirection)
+            {
+                skillSettings.length = EditorGUILayout.FloatField("Direction Length", skillSettings.length);
+                skillSettings.directionColor = EditorGUILayout.ColorField("Direction Color", skillSettings.directionColor);
+                skillSettings.directionSprite = EditorGUILayout.ObjectField("Direction Sprite", skillSettings.directionSprite, typeof(Sprite), true) as Sprite;
+               
+            }
+            
+        }
+    }
+
+}
