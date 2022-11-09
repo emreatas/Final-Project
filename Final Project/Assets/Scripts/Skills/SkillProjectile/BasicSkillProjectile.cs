1392024aedcb49a3fae825e38fc41c1e0ea22006
Serkan <yilserkan123@gmail.com>
2022-11-09 20:07:55 +0300

    refactored basic skill projectile

--- Final Project/Assets/Scripts/Skills/SkillProjectile/BasicSkillProjectile.cs
+++ Final Project/Assets/Scripts/Skills/SkillProjectile/BasicSkillProjectile.cs
@@ -1,45 +1,29 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Enemy;
 using UnityEngine;
 
 namespace Skills
 {
-    public class BasicSkillProjectile : MonoBehaviour
+    public class BasicSkillProjectile : AbstractProjectile
     {
-        [SerializeField] private float speed;
         [SerializeField] private float maxRange;
         
-        private Transform m_TargetTransform;
         private Vector3 m_PlayerForwardDir;
         private Vector3 m_SpawnPosition;
-        private float m_Damage;
         
-        private bool m_HasTarget = true;
-        
-        public void InitializeProjectile(Transform target, Vector3 playerForwardDir, float damage)
+        public override void FireProjectile(Vector3 dir)
         {
-            m_TargetTransform = target;
-            m_PlayerForwardDir = playerForwardDir;
+            m_PlayerForwardDir = dir;
             m_SpawnPosition = transform.position;
-            m_Damage = damage;
-            
-            if (m_TargetTransform == null)
-            {
-                m_HasTarget = false;
-            }
         }
         
         private void Update()
         {
-            if (m_HasTarget && m_TargetTransform != null)
+            if (m_HasTarget && !IsTargetDestroyed)
             {
-                transform.position = Vector3.MoveTowards(transform.position, m_TargetTransform.position, speed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, m_TargetTransform.position, m_AttackSpeed * Time.deltaTime);
             }
             else
             {
-                transform.position = Vector3.MoveTowards(transform.position, m_SpawnPosition + (m_PlayerForwardDir * maxRange), speed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, m_SpawnPosition + (m_PlayerForwardDir * maxRange), m_AttackSpeed * Time.deltaTime);
             }
         }
 
@@ -52,6 +36,8 @@ namespace Skills
                 Destroy(gameObject);
             }
         }
+
+       
     } 
 }
 
