ee959a016ca536ad3d1fcebdc02671932ffd299e
Saidt <saidsoykan@outlook.com>
2022-11-10 12:36:17 +0300

    Enemy Animations Work 1
    Enemy Idle and Move Animatons and Animator Created

--- Final Project/Assets/Scripts/Enemy/State Machine/EnemyIdleState.cs
+++ Final Project/Assets/Scripts/Enemy/State Machine/EnemyIdleState.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class EnemyIdleState : EnemyBaseState
 {
     public override void EnterState(EnemyStateManager enemy) {
-
+        enemy.anim.SetBool("isMove" , false);
+        enemy.getNavMeshAgent().isStopped = true;
     }
     public override void UpdateState(EnemyStateManager enemy) {
         if(Vector3.Distance(enemy.transform.position , enemy.getTargetTransform().position) <= enemy.enemyStats.sightRange) {
