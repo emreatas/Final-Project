76e808468f059dbcdc327f1825fbc4cb6ab46220
Serkan <yilserkan123@gmail.com>
2022-11-16 15:53:55 +0300

    random loot modified for testing

--- Final Project/Assets/Scripts/Enemy/RandomLoot.cs
+++ Final Project/Assets/Scripts/Enemy/RandomLoot.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Interactables;
 using Items;
 using UnityEngine;
 using Utils;
@@ -11,11 +12,26 @@ public class RandomLoot : AbstractSingelton<RandomLoot>
     [SerializeField] private ChestLoot loot;
 
     [SerializeField] private Chest chestPrefab;
-    
+
+    private void Start()
+    {
+        CreateLoot(Vector3.zero);
+        CreateLoot(Vector3.zero);
+        CreateLoot(Vector3.zero);
+    }
+
     public void CreateLoot(Vector3 position)
     {
         var chestLoot = Instantiate(loot);
         var instansiated = Instantiate(chestPrefab, position,Quaternion.identity);
         instansiated.InitializeChest(chestLoot);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            CreateLoot(Vector3.zero);
+        }
     }
 }
