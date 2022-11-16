936a0960085cf513a4aea931d7ff4101484a202b
Serkan <yilserkan123@gmail.com>
2022-11-16 15:50:35 +0300

    created player inventory ui

--- Final Project/Assets/Scripts/Item and Inventory/Inventorysystem/InventoryUI.cs
+++ Final Project/Assets/Scripts/Item and Inventory/Inventorysystem/InventoryUI.cs
@@ -1,21 +1,97 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utils;
 
 namespace  PInventory
 {
     public class InventoryUI : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
         
+        [SerializeField] private InventoryItemSlot inventoryItemSlotPrefab;
+        [SerializeField] private Transform inventorySlotParent;
+
+        private List<InventoryItemSlot> m_InventoryItemList = new List<InventoryItemSlot>();
+
+        public static GameEvent<Item> OnShowSelectedItem;
+        public static GameEvent<Item> OnRemoveItem;
+        
+        private void Start()
+        {
+            AddListeners();
+        } 
+        
+        private void OnDestroy()
+        {
+            RemoveListeners();
+        }
+        
+        private void HandleOnInventoryInitialized(List<InventoryItemData> inventoryItemDataList)
+        {
+            for (int i = 0; i < inventoryItemDataList.Count; i++)
+            {
+                var itemSlot = Instantiate(inventoryItemSlotPrefab, inventorySlotParent);
+                itemSlot.InitSlot(inventoryItemDataList[i], this);
+                
+                m_InventoryItemList.Add(itemSlot);
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void HandleOnItemAddedToInventory(InventoryUIData inventoryUIData)
         {
+            if (inventoryUIData.IsStacked)
+            {
+                for (int i = 0; i < m_InventoryItemList.Count; i++)
+                {
+                    if (m_InventoryItemList[i].ItemData.Item.ID == inventoryUIData.itemData.Item.ID)
+                    {
+                        m_InventoryItemList[i].SetItemCount(inventoryUIData.itemData.Count);
+                        return;
+                    }
+                }
+            }
+         
+            var instansiated = Instantiate(inventoryItemSlotPrefab,inventorySlotParent);
+            instansiated.InitSlot(inventoryUIData.itemData, this);
+            
+            m_InventoryItemList.Add(instansiated);
+        }
         
+        private void HandleOnItemRemovedFromInventory(InventoryItemData itemData)
+        {
+            for (int i = 0; i < m_InventoryItemList.Count; i++)
+            {
+                if (m_InventoryItemList[i].ItemData == itemData)
+                {
+                    m_InventoryItemList[i].SetItemCount(itemData.Count);
+                    return;
+                }
+            }
+        }
+        
+        public void RemoveItem(Item item)
+        {
+            OnRemoveItem.Invoke(item);
+        }
+
+        public void ShowItem(Item item)
+        {
+            OnShowSelectedItem.Invoke(item);
+        }
+        
+        private void AddListeners()
+        {
+            PlayerInventory.OnItemAddedToInventory.AddListener(HandleOnItemAddedToInventory);
+            PlayerInventory.OnInventoryInitialize.AddListener(HandleOnInventoryInitialized);
+            PlayerInventory.OnItemRemovedFromInventory.AddListener(HandleOnItemRemovedFromInventory);
+        }
+        
+        private void RemoveListeners()
+        {
+            PlayerInventory.OnItemAddedToInventory.RemoveListener(HandleOnItemAddedToInventory);
+            PlayerInventory.OnInventoryInitialize.RemoveListener(HandleOnInventoryInitialized);
+            PlayerInventory.OnItemRemovedFromInventory.RemoveListener(HandleOnItemRemovedFromInventory);
         }
     }
 
