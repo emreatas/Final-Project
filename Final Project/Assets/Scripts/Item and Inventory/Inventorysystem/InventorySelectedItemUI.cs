8342198e6cc5de447afc3f02a73201fdecb3b2b7
Serkan <yilserkan123@gmail.com>
2022-11-16 15:50:59 +0300

    created inventory selected item ui

---
+++ Final Project/Assets/Scripts/Item and Inventory/Inventorysystem/InventorySelectedItemUI.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PInventory;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySelectedItemUI : MonoBehaviour
+{
+    [SerializeField] private Image iconImage;
+    [SerializeField] private Image tierImage;
+    [SerializeField] private TextMeshProUGUI itemNameText;
+    [SerializeField] private List<TextMeshProUGUI> itemStatTextList = new List<TextMeshProUGUI>();
+
+    private Item m_SelectedItem;
+    
+    private void Start()
+    {
+        AddListeners();
+    }
+
+    private void OnDestroy()
+    {
+        RemoveListeners();
+    }
+
+    private void HandleOnShowSelectedItem(Item item)
+    {
+        m_SelectedItem = item;
+        
+        iconImage.sprite = item.Icon;
+        tierImage.sprite = item.TierImage;
+        itemNameText.text = item.ItemName;
+
+        for (int i = 0; i < item.stats.Count; i++)
+        {
+            itemStatTextList[i].text = item.stats[i].GetText();
+        }
+        
+        EnableUIElements();
+    }
+
+    private void ShowUIButtons()
+    {
+        
+    }
+
+    private void EnableUIElements()
+    {
+        iconImage.enabled = true;
+        tierImage.enabled = true;
+    }
+
+    private void DisableUIElements()
+    {
+        ResetUIElements();
+        
+        iconImage.enabled = false;
+        tierImage.enabled = false;
+    }
+
+    private void ResetUIElements()
+    {
+        iconImage.sprite = null;
+        tierImage.sprite = null;
+
+        itemNameText.text = "";
+
+        for (int i = 0; i < itemStatTextList.Count; i++)
+        {
+            itemStatTextList[i].text = "";
+        }
+    }
+    
+    private void AddListeners()
+    {
+        PInventory.InventoryUI.OnShowSelectedItem.AddListener(HandleOnShowSelectedItem);
+    }
+
+    private void RemoveListeners()
+    {
+        PInventory.InventoryUI.OnShowSelectedItem.RemoveListener(HandleOnShowSelectedItem);
+    }
+}
