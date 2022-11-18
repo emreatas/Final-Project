bc8b50242d7171bd398f2f6e0acc70057b010b7e
Serkan <yilserkan123@gmail.com>
2022-11-18 18:14:28 +0300

    created inventory stat ui

---
+++ Final Project/Assets/Scripts/Item and Inventory/Inventorysystem/Inventory/InventoryStatUI.cs
@@ -0,0 +1,40 @@
+using System;
+using Player;
+using Stat;
+using UnityEngine;
+
+namespace PInventory
+{
+    public class InventoryStatUI : MonoBehaviour
+    {
+        [SerializeField] private InventoryStatSlot[] statSlots;
+
+        private void Start()
+        {
+            AddListeners();
+        }
+
+        private void OnDestroy()
+        {
+            RemoveListeners();
+        }
+
+        private void HandleOnCharacterStatsInitialized(CharacterStat characterStats)
+        {
+            for (int i = 0; i < statSlots.Length; i++)
+            {
+                statSlots[i].SetStatText(characterStats.GetValue(statSlots[i].StatType).ToString());
+            }
+        }
+        
+        private void AddListeners()
+        {
+            PlayerStats.OnCharacterStatsInitialized.AddListener(HandleOnCharacterStatsInitialized);
+        }
+
+        private void RemoveListeners()
+        {
+            PlayerStats.OnCharacterStatsInitialized.RemoveListener(HandleOnCharacterStatsInitialized);
+        }
+    }
+}
