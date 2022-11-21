1fa7ccff70fa5197c478bd71e9a11a5d7b10a3a1
Serkan <yilserkan123@gmail.com>
2022-11-21 12:55:17 +0300

    modified test skill settings
    added particle system

--- Final Project/Assets/Scripts/Skills/SkillSettings/SkillSettingsEmpty.cs
+++ Final Project/Assets/Scripts/Skills/SkillSettings/SkillSettingsEmpty.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/Skills/Empty")]
 public class SkillSettingsEmpty : AbstractSkillSettings
 {
+    [SerializeField] private ParticleSystem skill;
+ 
     public override void CastSkill()
     {
-      
+        Instantiate(skill, m_Player);
     }
 
     public override void OnFinishedSkillAnimation()
