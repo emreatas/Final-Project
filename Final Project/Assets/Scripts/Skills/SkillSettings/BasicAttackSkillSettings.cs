520b56d21407d1ef7607b00c2564e2353e3a43ff
Serkan <yilserkan123@gmail.com>
2022-11-24 10:29:22 +0300

    modified asset files

---
+++ Final Project/Assets/Scripts/Skills/SkillSettings/BasicAttackSkillSettings.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using ObjectPooling;
+using Player;
+using UnityEngine;
+
+namespace Skills
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Skills/New/WarriorAttack")]
+    public class BasicAttackSkillSettings : AbstractNewSkillSettings
+    {
+        [SerializeField] private AbstractNewSkill firstComboSkill;
+        [SerializeField] private AbstractNewSkill secondComboSkill;
+        [SerializeField] private AbstractNewSkill thirdComboSkill;
+        
+        private int m_ComboCount;
+        
+        public override void StartSkill(PlayerSkillController skillController)
+        {
+            m_ComboCount = skillController.PlayerAnimationController.GetComboCount;
+        }
+
+        public override void ExecuteSkill(PlayerSkillController skillController)
+        {
+            if (m_ComboCount == 1)
+            {
+                PoolSkill(skillController, ref firstComboSkill);
+            }
+            else if (m_ComboCount == 2)
+            {
+                PoolSkill(skillController, ref secondComboSkill);
+            }
+            else 
+            {
+                PoolSkill(skillController, ref thirdComboSkill);
+            }
+        }
+
+        private void PoolSkill(PlayerSkillController skillController, ref AbstractNewSkill skill)
+        {
+            var pooled = SkillPool.Instance.PoolSkill(skill);
+            pooled.SetPositionAndRotation(skillController.transform.position, Quaternion.LookRotation(skillController.transform.forward));
+            pooled.InitSkill(skillController);
+        }
+    }
+
+}
