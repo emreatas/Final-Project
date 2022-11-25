29918c5f5ccd481b6c8d7fa48045d50bc778cf13
Serkan <yilserkan123@gmail.com>
2022-11-25 11:22:44 +0300

    created animation controller selecotr
    this script selects the animation controller from the selected character type

---
+++ Final Project/Assets/Scripts/Player/PlayerAnimationControllerSelector.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Stat;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAnimationControllerSelector : MonoBehaviour
+    {
+        [SerializeField] private Animator playerAnimator;
+        
+        [SerializeField] private RuntimeAnimatorController mageAnimController;
+        [SerializeField] private RuntimeAnimatorController archerAnimController;
+        [SerializeField] private RuntimeAnimatorController warriorAnimController;
+
+        private void OnEnable()
+        {
+            AddListeners();
+        }
+
+        private void OnDisable()
+        {
+            RemoveListeners();
+        }
+        
+        private void HandleOnCharacterInitialized(PlayerSettings playerSettings)
+        {
+            SelectCorrectAnimatonController(playerSettings);
+        }
+
+        private void SelectCorrectAnimatonController(PlayerSettings playerSettings)
+        {
+            switch (playerSettings.characterType)
+            {
+                case CharacterTypes.Mage:
+                    playerAnimator.runtimeAnimatorController = mageAnimController;
+                    break;
+                case CharacterTypes.Warrior:
+                    playerAnimator.runtimeAnimatorController = warriorAnimController;
+                    break;
+                case CharacterTypes.Archer:
+                    playerAnimator.runtimeAnimatorController = archerAnimController;
+                    break;
+            }
+        }
+
+        private void AddListeners()
+        {
+            PlayerClass.OnCharacterInitialized.AddListener(HandleOnCharacterInitialized);
+        }
+        
+        private void RemoveListeners()
+        {
+            PlayerClass.OnCharacterInitialized.RemoveListener(HandleOnCharacterInitialized);
+        }
+    }
+}
