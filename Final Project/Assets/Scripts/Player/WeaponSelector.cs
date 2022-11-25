6466b1187e3e0c6e834cb803e57299bc61553b53
Serkan <yilserkan123@gmail.com>
2022-11-25 11:11:33 +0300

    modified player weaponselector
    The weapon of the selectred character type gets activated

--- Final Project/Assets/Scripts/Player/WeaponSelector.cs
+++ Final Project/Assets/Scripts/Player/WeaponSelector.cs
@@ -1,10 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using Stat;
+using UnityEngine;
 
 namespace Player
 {
     public class WeaponSelector : MonoBehaviour
     {
+        [SerializeField] private GameObject mageWeapon;
+        [SerializeField] private GameObject archerWeapon;
+        [SerializeField] private GameObject warriorWeapon;
+        
         [SerializeField] private Weapon[] weaponsParent;
+
+
+        private void OnEnable()
+        {
+            AddListeners();
+        }
+
+        private void OnDisable()
+        {
+            RemoveListeners();
+        }
+
+        private void HandleOnCharacterInitialized(PlayerSettings playerSettings)
+        {
+            EnableSelectedCharacterWeapon(playerSettings);
+        }
 
         public Transform GetActiveWeaponTransform()
         {
@@ -17,6 +39,32 @@ namespace Player
             }
 
             return null;
+        }
+
+        private void EnableSelectedCharacterWeapon(PlayerSettings playerSettings)
+        {
+            switch (playerSettings.characterType)
+            {
+                case CharacterTypes.Mage:
+                    mageWeapon.SetActive(true);
+                    break;
+                case CharacterTypes.Warrior:
+                    warriorWeapon.SetActive(true);
+                    break;
+                case CharacterTypes.Archer:
+                    archerWeapon.SetActive(true);
+                    break;
+            }
+        }
+
+        private void AddListeners()
+        {
+            PlayerClass.OnCharacterInitialized.AddListener(HandleOnCharacterInitialized);
+        }
+
+        private void RemoveListeners()
+        {
+            PlayerClass.OnCharacterInitialized.AddListener(HandleOnCharacterInitialized);
         }
     }
 }
