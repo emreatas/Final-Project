da99c42b071b0a779c5e16979c38d6b778eecfd8
Serkan <yilserkan123@gmail.com>
2022-11-25 11:03:17 +0300

    created skill container settings
    This contains all skills for a specific character type.

---
+++ Final Project/Assets/Scripts/Skills/SkillSettings/SkillSettingsContainer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Skills
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Skills/SkillContainer")]
+    public class SkillSettingsContainer : ScriptableObject
+    {
+        [Header("Basic Attack")]
+        public AbstractNewSkillSettings Combo1;
+        public AbstractNewSkillSettings Combo2;
+        public AbstractNewSkillSettings Combo3;
+        
+        [Header("Primary Skills")]
+        public AbstractNewSkillSettings Primary1;
+        public AbstractNewSkillSettings Primary2;
+        public AbstractNewSkillSettings Primary3;
+        
+        [Header("Secondary Skills")]
+        public AbstractNewSkillSettings Secondary1;
+        public AbstractNewSkillSettings Secondary2;
+        public AbstractNewSkillSettings Secondary3;
+    }
+}
