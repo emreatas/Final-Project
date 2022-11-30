8b401990016e7dfde0fbed384b04414c03c8cbb2
Emre Ataş <emreatas1@hotmail.com>
2022-11-30 19:05:34 +0300

    stat tiplerine id eklendi
    stat tiplerine jsonda dosyasında kayıt ederken instanceID tutmak yerine bir id atandı

--- Final Project/Assets/Scripts/Stats/StatType.cs
+++ Final Project/Assets/Scripts/Stats/StatType.cs
@@ -6,8 +6,9 @@ namespace Stat
     [CreateAssetMenu(menuName = "ScriptableObjects/Stats/Type")]
     public class StatType : ScriptableObject
     {
+        public int ID;
         public string name;
-        [TextArea(10,100)]
+        [TextArea(10, 100)]
         public string description;
     }
 }
